33e5a59d3717ee467dab2a5f9ce4afeab39680df
Tina Lee <tinalee262008@gmail.com>
2020-11-22 16:15:06 -0800

    Update Drawer

--- src/client/project/App.js
+++ src/client/project/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import { createDrawerNavigation, createDrawerNavigator } from '@react-navigation/drawer';
+import {
+	createDrawerNavigator,
+	DrawerContentScrollView,
+	DrawerItem,
+} from '@react-navigation/drawer';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Icon } from 'react-native-elements';
+
+function CustomDrawerContent(props) {
+	return (
+		<SafeAreaProvider>
+			<DrawerContentScrollView {...props}>
+				<DrawerItem
+					label="Home"
+					labelStyle={styles.labelSize}
+					icon={() => <Icon name={'home'} size={30} color={'#F1FAEE'} />}
+					onPress={() => props.navigation.navigate('Home')}
+				/>
+				<DrawerItem
+					label="Settings"
+					labelStyle={styles.labelSize}
+					icon={() => <Icon name={'settings'} size={30} color={'#F1FAEE'} />}
+					onPress={() => {}}
+				/>
+			</DrawerContentScrollView>
+		</SafeAreaProvider>
+	);
+}
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        initialRouteName="Home"
-        drawerStyle={{
-          backgroundColor: '#2B5085'
-        }}
-        drawerContentOptions={{
-          labelStyle: {color: '#F1FAEE', fontSize: 22, fontWeight: "bold"}
-        }}
-      >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator
+				initialRouteName="Home"
+				drawerStyle={{
+					backgroundColor: '#2B5085',
+				}}
+				drawerContent={props => <CustomDrawerContent {...props} />}
+			>
+				<Drawer.Screen name="Home" component={HomeScreen} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 }
+
+const styles = StyleSheet.create({
+	labelSize: {
+		color: '#F1FAEE',
+		fontSize: 22,
+		fontWeight: 'bold',
+	},
+});
--- src/client/project/SearchBar.js
+++ src/client/project/SearchBar.js
@@ -1,55 +1,55 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import { Icon } from 'react-native-elements';
+import { SearchBar, Icon } from 'react-native-elements';
 
 export default class SearchApp extends React.Component {
-  state = {
-    search: '',
-  };
+	state = {
+		search: '',
+	};
 
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
+	updateSearch = search => {
+		this.setState({ search });
+	};
 
-  render() {
-    const { search } = this.state;
+	render() {
+		const { search } = this.state;
 
-    return (
-      <SearchBar
-        placeholder="Search"
-        placeholderTextColor='#F1FAEE'
-        platform={ Platform.OS }
-        onChangeText={ this.updateSearch }
-        value={ search }
-        clearIcon = { false }
-        cancelButtonProps={{
-          color: '#F1FAEE'
-        }}
-        searchIcon={ 
-          <Icon 
-            name="menu" 
-            size={30} 
-            color='#F1FAEE' 
-            onPress={() => this.props.navigation.openDrawer()}/> 
-        }
-        inputStyle={{ backgroundColor: '#457B9D' }}
-        containerStyle={{
-          backgroundColor: '#457B9D',
-          borderBottomColor: 'transparent',
-          borderTopColor: 'transparent',
-          left: 14,
-          width: 348,
-          height: 60,
-          borderWidth: 0,
-          borderRadius: 10,
-          marginTop: 45
-        }}
-        inputContainerStyle={{
-          backgroundColor: '#457B9D'
-        }}
-        inputStyle={{ color: '#F1FAEE' }}
-      />
-    );
-  }
+		return (
+			<SearchBar
+				placeholder="Search"
+				placeholderTextColor="#F1FAEE"
+				platform={Platform.OS}
+				onChangeText={this.updateSearch}
+				value={search}
+				clearIcon={false}
+				cancelButtonProps={{
+					color: '#F1FAEE',
+				}}
+				searchIcon={
+					<Icon
+						name="menu"
+						size={30}
+						color="#F1FAEE"
+						onPress={() => this.props.navigation.openDrawer()}
+					/>
+				}
+				inputStyle={{ backgroundColor: '#457B9D' }}
+				containerStyle={{
+					backgroundColor: '#457B9D',
+					borderBottomColor: 'transparent',
+					borderTopColor: 'transparent',
+					left: 14,
+					width: 348,
+					height: 60,
+					borderWidth: 0,
+					borderRadius: 10,
+					marginTop: 45,
+				}}
+				inputContainerStyle={{
+					backgroundColor: '#457B9D',
+				}}
+				inputStyle={{ color: '#F1FAEE' }}
+			/>
+		);
+	}
 }
